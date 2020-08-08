876028e70e5623272af050d0a719bb34685c6d35
IzzyBeraja <20325290+IzzyBeraja@users.noreply.github.com>
2020-08-08 03:25:30 -0400

    Created two new components: Like and pagination. Moved them to a new folder: Common

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import _ from "lodash";
+
+const Pagination = props => {
+  const { itemsCount, itemsPerPage, currentPage, onPageChange } = props;
+  if (itemsPerPage <= 0 || itemsPerPage > itemsCount) return null;
+  const pagesCount = Math.ceil(itemsCount / itemsPerPage);
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination" style={{ justifyContent: "center" }}>
+        {pages.map(page => {
+          return (
+            <li
+              key={page}
+              className={"page-item" + (page === currentPage && " active")}
+              onClick={() => onPageChange(page)}
+              style={{ cursor: "pointer" }}
+            >
+              <p className="page-link">{page}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
