32db7b0db5e9bb06deca77f9e35e7a7163a4c978
IzzyBeraja <20325290+IzzyBeraja@users.noreply.github.com>
2020-08-08 11:20:46 -0400

    Imported PropTypes. Added propTypes to Pagination component.

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 
 const Pagination = props => {
@@ -25,6 +26,13 @@ const Pagination = props => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  itemsPerPage: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
